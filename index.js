d2534748f91c0062f9d53787a8b9080eb09b5029
GameCatCam <cameronrawlins11@gmail.com>
2023-11-17 00:10:27 -0500

    turned main inquirer prompt and one follow-up into async functions. (Works way better)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const util = require('util');
 
 const db = mysql.createConnection(
     {
@@ -11,53 +12,101 @@ const db = mysql.createConnection(
     console.log('Connected to the employees_db!')
 );
 
-function viewDepartments() {
-    db.query('SELECT * FROM department', (err, result) => {
-            console.log(result)
-        })
+const queryAsync = util.promisify(db.query).bind(db);
+
+async function init() {
+    try {
+        const answers = await inquirer.prompt([
+            {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update an employee role',
+                'quit'
+            ]
+            }
+        ]);
+  
+        switch (answers.choice) {
+            case 'view all departments':
+            await viewDepartments();
+            break;
+            case 'view all roles':
+            await viewRoles();
+            break;
+            case 'view all employees':
+            await viewEmployees();
+            break;
+            case 'add a department':
+            await addDepartment();
+            break;
+            case 'add a role':
+            await addRole();
+            break;
+            case 'add an employee':
+            await addEmployee();
+            break;
+            case 'quit':
+            return false;
+            default:
+            updateEmployee();
+        }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      init();
+    }
 }
 
-function init() {
-    db.query('SELECT * FROM department', (err, result) => {
-        console.log(result)
+async function viewDepartments() {
+    try {
+      const result = await queryAsync('SELECT * FROM department');
+      console.table(result);
+    } catch (err) {
+      console.error(err);
+    }
+}
+
+function viewRoles() {
+    db.query('SELECT * from roles', (err, result) => {
+        console.table(result)
     })
+    init()
 }
 
-/*function init() {
+function viewEmployees() {
+    db.query('SELECT * from employees', (err, result) => {
+        console.table(result)
+    })
+    init()
+}
+
+function addDepartment() {
     inquirer
         .prompt([
             {
-                type: 'list',
-                message: 'Welcome to Employee Database! What would you like to do?',
-                name: 'choice',
-                choices: [
-                    'view all departments',
-                    'view all roles',
-                    'view all employees',
-                    'add a department',
-                    'add a role',
-                    'add an employee',
-                    'update an employee role'
-                ]
+                type: 'input',
+                message: 'What is the name of the new Department?',
+                name: 'department'
             }
         ])
         .then((answers) => {
-            if (`${answers.choice}` === 'view all departments') {
-                viewDepartments();
-            } else if (`${answers.choice}` === 'view all roles') {
-                viewRoles();
-            } else if (`${answers.choice}` === 'view all employees') {
-                viewEmployees();
-            } else if (`${answers.choice}` === 'add a department') {
-                addEmployee();
-            } else if (`${answers.choice}` === 'add a role') {
-                addRole();
-            } else if (`${answers.choice}` === 'add an employee') {
-                addEmployee();
-            } else {
-                updateEmployee();
-            }
+            console.log(answers.department)
+            /*db.query(
+                `INSERT INTO department(dep_name) VALUES ('${answers.department}')`
+                )*/
         })
-}*/
+        .then(
+            init()
+        ) 
+    
+}
 
 init()
